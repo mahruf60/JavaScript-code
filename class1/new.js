076b74313cd41a9b5cc70418ea3b60be2afe53d8
mahruf60 <mahruf9060@gmail.com>
2024-11-21 22:43:13 +0600

    Introduction to JavaScript & node js.

---
+++ class1/new.js
@@ -0,0 +1,4 @@
+console.log("Hello World")
+console.log("Code running...")
+console.log("Code is also running...")
+console.log("Code is looking like wow...")
---
+++ class1/script.js
@@ -0,0 +1,20 @@
+alert("Hello,World")
+
+console.log("Code running...")
+console.log("Code is also running...")
+console.log("Code is looking like wow...")
+
+var a = prompt("Enter you number:")
+var isTrue = confirm("Are you sure you want to leave this page and blast your computer.")
+if (isTrue){
+    console.log("Your computer is Blasting.")
+}
+else{
+    console.log("Computer is not Blasting.")
+}
+
+console.log("Your number is:" + a)
+
+document.title = "Hey i am good"
+
+// document.body.style.backgroundColor = "red"
