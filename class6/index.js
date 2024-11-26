82bfae358c3cb1e073c0c07160fc1f3aac2c033f
mahruf60 <mahruf9060@gmail.com>
2024-11-27 01:19:22 +0600

    JavaScript Strings

---
+++ class6/index.js
@@ -0,0 +1,29 @@
+console.log("This is string tutorial.")
+
+let a = "Shuvo"
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4]) 
+// console.log(a[5])
+console.log(a.length) 
+
+let real_name = "Shuvo"
+let friend = "Ehsan"
+console.log("His name is "+real_name+" and his friend name is "+friend)
+// Template Literals in JavaScripts
+console.log(`His name is ${real_name} and his friend name is ${friend}`)
+
+let b = "Sarjil"
+console.log(b.toUpperCase())  
+console.log(b.toLowerCase()) 
+
+console.log(b.slice(1,4))
+console.log(b.slice(1))
+
+console.log(b.replace("Sa","Ma"))
+
+console.log(b.concat(a,"Ehsan","Sabbir","Sifat","Imran"))
+
+console.log(b.charAt(2))
